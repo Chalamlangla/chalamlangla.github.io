66e04afc5f647a9eae309132787bb90d8c4d2ef8
Lou <84123807+Chalamlangla@users.noreply.github.com>
2025-05-07 14:54:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function poga0() {
+    window.location.href = "latval.html"
+}
+function poga1() {
+    window.location.href = "angval.html"
+}
+function poga2() {
+    window.location.href = "matematika.html"
+}
+function poga3() {
+    window.location.href = "index.html"
+}
